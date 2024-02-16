65bd335d1b2a6824aca036bd6c615ee212817664
Sudarshan S <senthilsudarshan@gmail.com>
2024-02-16 20:50:45 +0700

    footer init + consts

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Image from "next/image"
+import React from "react"
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+	return (
+		<footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+			<div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+				<Image
+					src="/logo.svg"
+					alt="logo"
+					width={118}
+					height={18}
+					className="object-contain"
+				/>
+        <p className="text-base text-gray-700">
+          Carhub 2023 <br />
+          All rights reserved &copy;
+        </p>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
